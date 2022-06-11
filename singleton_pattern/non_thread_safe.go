b25bf265ed4969ce05af01a48ce619ba1146e9c3
rahul-yr <yarragodularahulreddy@gmail.com>
2022-06-11 17:32:23 +0530

    Added comments

--- singleton_pattern/non_thread_safe.go
+++ singleton_pattern/non_thread_safe.go
@@ -33,20 +33,29 @@ func performConcurrentAction() {
 	// This is essentially needed for waiting for the program
 	// to finish its concurrent tasks before exiting the program in Go.
 	var wg sync.WaitGroup
-
+	// iterate over 10 times
+	// and call the mockConnectionNonThreadSafe function
+	// concurrently
 	for i := 0; i < 10; i++ {
+		// add 1 to the wait group
 		wg.Add(1)
 		go func(threadId int) {
+			// defer is used to ensure that the wait group is
+			// decremented after the goroutine completes
+			// this is done to ensure that the program doesn't
+			// exit before all the goroutines complete
 			defer wg.Done()
 			log.Println("thread id:", threadId)
 			mockConnectionNonThreadSafe(threadId)
 		}(i)
 	}
-
+	// wait for all the goroutines to complete
 	wg.Wait()
 }
 
 func performSequentialAction() {
+	// iterate over 10 times
+	// and call the mockConnectionNonThreadSafe function
 	for i := 0; i < 10; i++ {
 		mockConnectionNonThreadSafe(i)
 	}
@@ -54,6 +63,8 @@ func performSequentialAction() {
 
 // main is the entry point for the application.
 func main() {
-	performConcurrentAction()
-	// performSequentialAction()
+	// below function call is for concurrent execution
+	// performConcurrentAction()
+	// below function call is used to execute the sequential action
+	performSequentialAction()
 }
--- singleton_pattern/thread_safe.go
+++ singleton_pattern/thread_safe.go
@@ -39,20 +39,29 @@ func performConcurrentAction() {
 	// This is essentially needed for waiting for the program
 	// to finish its concurrent tasks before exiting the program in Go.
 	var wg sync.WaitGroup
-
+	// iterate over 10 times
+	// and call the mockConnectionThreadSafe function
 	for i := 0; i < 10; i++ {
+		// add 1 to the wait group
 		wg.Add(1)
 		go func(threadId int) {
+			// defer is used to ensure that the wait group is
+			// decremented after the goroutine completes
+			// this is done to ensure that the program doesn't
+			// exit before all the goroutines complete
 			defer wg.Done()
 			log.Println("thread id:", threadId)
+			// This is a blocking call to mimic the time it takes
 			mockConnectionThreadSafe(threadId)
 		}(i)
 	}
-
+	// wait for all the goroutines to complete
 	wg.Wait()
 }
 
 func performSequentialAction() {
+	// iterate over 10 times
+	// and call the mockConnectionThreadSafe function
 	for i := 0; i < 10; i++ {
 		mockConnectionThreadSafe(i)
 	}
@@ -60,6 +69,10 @@ func performSequentialAction() {
 
 // main is the entry point for the application.
 func main() {
+	// This is a call for the concurrent execution of the
+	// mockConnectionThreadSafe function.
 	performConcurrentAction()
+	// This is a call for the sequential execution of the
+	// mockConnectionThreadSafe function.
 	// performSequentialAction()
 }
